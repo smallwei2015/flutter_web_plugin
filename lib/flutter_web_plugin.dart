c2757f58f3be1e06b421ac5be4438d54c6786d5c
marionette <smallwei2015@gmail.com>
2025-04-02 15:56:10 +0800

    Add main plugin file

---
+++ lib/flutter_web_plugin.dart
@@ -0,0 +1,7 @@
+import 'flutter_web_plugin_platform_interface.dart';
+
+class FlutterWebPlugin {
+  Future<String?> getPlatformVersion() {
+    return FlutterWebPluginPlatform.instance.getPlatformVersion();
+  }
+}
